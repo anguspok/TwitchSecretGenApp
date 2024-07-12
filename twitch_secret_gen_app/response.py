f02708660a8265960cb854f13f57160a659678f6
Angus Pok <anguspokhl@gmail.com>
2024-07-12 09:12:54 +0800

    Update response.py
    - completed response_reader method
    - added and completed response_gen method

--- twitch_secret_gen_app/response.py
+++ twitch_secret_gen_app/response.py
@@ -1,41 +1,62 @@
-rom random import randint
-from ResponseClass import *
+from pathlib import Path
+from random import randint
 
 
-class Parent:
+class Response:
     '''
-    '''
-    pass
-
-
-class NumGenClass:
-    '''
-    Class with methods for generating a random number
+    Class with methods for reading chat guesses and returning a response
 
     Methods
     -------
-    response_num_gen():
-        generates a random number corresponding to index of elements in response_list
-    '''
+    response_gen(response_list):
+         randomly chooses a response from response_list
 
-    def response_num_gen(self):
+    give_response(response)
+    '''
+    filepath = Path(r'FILEPATH')
+
+    def response_reader(self, filepath: str) -> list:
+        '''
+        reads from response.txt
+            Parameters:
+            filepath (str): absolute file path of response.txt
+
+            Returns:
+            response_list (list): List of responses
+        '''
+        response_file = open(filepath, "rt")
+        
+        # instantiate empty list
+        response_list = []
+
+        # read all lines in response.txt
+        lines = response_file.readlines()
+
+        # add each line from response.txt as an item into response_list
+        for line in lines:
+            response_list.append(line.strip()) #remove whitespaces
+
+        return response_list
+
+    
+    def response_gen(self, response_list) -> str:
         '''
         generates a random number corresponding to index of elements in response_list
             Parameters:
-
+                response_list (list): List of responses
             Returns:
-                gen_num (int): Integer which corresponds to index of elements in response_list
+                gen_response (str): Randomly chosen response
         '''
-        # response_end_index is the upper limit of random int range
-        response_end_index = len(ResponseClass.response_list) - 1
+        # upper limit of random int range
+        end_index = len(response_list) - 1
 
         # get random integer between range
-        gen_num = randint(0, response_end_index)
+        counter = 0
+        while counter < 100:
+            gen_num = randint(0, end_index)
+            counter+=1
 
-        return gen_num
+        # get response from response_list
+        gen_response = response_list[gen_num]
 
-
-class ResponseReader(ParentTxtReader):
-    '''
-    '''
-    pass
+        return gen_response
